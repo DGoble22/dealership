a3a412cd7407479f09a8da168426ceab461d15dd
Dylan Goble <dgoble0516@gmail.com>
2026-02-02 11:52:42 -0500

    fix: fixed missing css from removing extra code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,6 @@ function App() {
                     <Routes>
                         <Route path="/" element={<Inventory isAdmin={isAdmin} />} />
                         <Route path="/AboutUs" element={<AboutUs/>} />
-                        <Route path="/car/:id" element={<CarDetails/>} />
                     </Routes>
                 </main>
             </div>
